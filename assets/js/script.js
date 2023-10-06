febce808599184051b98d82b180ba753ca186674
mutiat <mutiat900@gmail.com>
2023-10-06 23:40:39 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,26 +26,31 @@ function toggleHambuger() {
 
 const products = [
   {"img":"assets/images/products/1.jpg", "discount":"5%", "description":"Unpaired Gray Nike Running Shoe", "price":"200", "gender":"male"},
-  {"img":"assets/images/products/2.jpg", "discount":"5%", "description":"White tank top", "price":"25", "gender":"male"},
-  {"img":"assets/images/products/3.jpg", "discount":"5%", "description":"Yellow and black bikini", "price":"50", "gender":"male"},
-  {"img":"assets/images/products/4.jpg", "discount":"5%", "description":"hite top and stunt jacket", "price":"250", "gender":"female"},
-  {"img":"assets/images/products/5.jpg", "discount":"5%", "description":"Main brown leather coat", "price":"100", "gender":"babies"},
-  {"img":"assets/images/products/6.jpg", "discount":"5%", "description":"Pair of white-and-orange athletic shoes on white box", "price":"90", "gender":"female"},
+  {"img":"assets/images/products/2.jpg", "discount":"5%", "description":"White tank top", "price":"25", "gender":"female"},
+  {"img":"assets/images/products/3.jpg", "discount":"5%", "description":"Yellow and black bikini", "price":"50", "gender":"female"},
+  {"img":"assets/images/products/4.jpg", "discount":"5%", "description":"hite top and stunt jacket", "price":"250", "gender":"male"},
+  {"img":"assets/images/products/5.jpg", "discount":"5%", "description":"Main brown leather coat", "price":"100", "gender":"male"},
+  {"img":"assets/images/products/6.jpg", "discount":"5%", "description":"Pair of white-and-orange athletic shoes on white box", "price":"90", "gender":"male"},
   {"img":"assets/images/products/7.jpg", "discount":"5%", "description":"White Nike running shoes standing", "price":"110", "gender":"male"},
-  {"img":"assets/images/products/8.jpg", "discount":"5%", "description":"Assorted-color shirt lot hang on rack", "price":"500", "gender":"male"},
+  {"img":"assets/images/products/8.jpg", "discount":"5%", "description":"Assorted-color shirt lot hang on rack", "price":"500", "gender":"female"},
   {"img":"assets/images/products/9.jpg", "discount":"5%", "description":"Red and white crew neck t-shirt and white pants", "price":"90", "gender":"male"},
-  {"img":"assets/images/products/10.jpg", "discount":"5%", "description":"View the photo by Mojtaba Fahiminia", "price":"240", "gender":"female"},
-  {"img":"assets/images/products/11.jpg", "discount":"5%", "description":"Orange and white headdress", "price":"30", "gender":"babies"},
+  {"img":"assets/images/products/10.jpg", "discount":"5%", "description":"View the photo by Mojtaba Fahiminia", "price":"240", "gender":"male"},
+  {"img":"assets/images/products/11.jpg", "discount":"5%", "description":"Orange and white headdress", "price":"30", "gender":"female"},
   {"img":"assets/images/products/12.jpg", "discount":"5%", "description":"White and blue nike air force 1 high", "price":"300", "gender":"male"},
   {"img":"assets/images/products/13.jpg", "discount":"5%", "description":"Blue jacket and a black pant", "price":"270", "gender":"male"},
   {"img":"assets/images/products/14.jpg", "discount":"5%", "description":"Black, white, and gray textiles", "price":"99", "gender":"male"},
   {"img":"assets/images/products/15.jpg", "discount":"5%", "description":"Two black top", "price":"95", "gender":"male"},
-  {"img":"assets/images/products/16.jpg", "discount":"5%", "description":"White beach dress", "price":"220", "gender":"babies"},
+  {"img":"assets/images/products/16.jpg", "discount":"5%", "description":"White beach dress", "price":"220", "gender":"female"},
 ];
 
 window.document.onreadystatechange = () =>{
     renderProducts(products);
 };
+
+// ==================================
+// 3. Display products based on 
+// search keywords in the input fields.
+// ==================================
 
 function search(query) {
   const search_result = products.map(product=>{
@@ -83,8 +88,3 @@ function renderProducts(products) {
 
 
 
-
-// ==================================
-// 3. Display products based on 
-// search keywords in the input fields.
-// ==================================
